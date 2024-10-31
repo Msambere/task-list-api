4ce326896401cc61571fdf1bc9dd2dc16dd7dfa6
Amber <110812972+Msambere@users.noreply.github.com>
2024-10-31 15:50:33 -0500

    Create task model

--- app/models/task.py
+++ app/models/task.py
@@ -6,4 +6,4 @@ class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
-    completed_at:Mapped[datetime]
+    completed_at:Mapped[datetime] = mapped_column(default=None, nullable=True)
