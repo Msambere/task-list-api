d332b3fe1eb2717fe2b6d37d75fc04bfe0c23034
Amber <110812972+Msambere@users.noreply.github.com>
2024-11-01 11:31:41 -0500

    Complete Wave 3
    Functions and routes to edit complete_at attribute of tasks was created.

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, abort, make_response, request, Response
 from app.models.task import Task
 from app.db import db
 from sqlalchemy import desc
+import datetime
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
@@ -65,6 +66,26 @@ def delete_task(task_id):
     }
     return response, 200
 
+@tasks_bp.patch("/<task_id>/mark_complete")
+def mark_task_complete(task_id):
+    task = validate_task_id(task_id)
+
+    task.completed_at = datetime.datetime.now()
+    db.session.commit()
+
+    return {
+        "task":task.to_dict()
+        }, 200
+
+@tasks_bp.patch("/<task_id>/mark_incomplete")
+def mark_task_incomplete(task_id):
+    task = validate_task_id(task_id)
+    task.completed_at = None
+    db.session.commit()
+
+    return {
+        "task":task.to_dict()
+        }, 200
 
 # Helper Function
 def validate_task_id(task_id):
