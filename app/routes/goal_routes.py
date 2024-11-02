76673dca9bdc67aa79495f84808932410814c600
Amber <110812972+Msambere@users.noreply.github.com>
2024-11-01 21:18:34 -0500

    Create post route for create_goal()

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -8,4 +8,28 @@ import os
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
+@goals_bp.post("")
+def create_goal():
+    request_body = request.get_json()
+    title = validate_new_goal_data(request_body)
 
+    new_goal = Goal(title=title)
+    db.session.add(new_goal)
+    db.session.commit()
+
+    response = {"goal": new_goal.to_dict()}
+    return response, 201
+
+
+# Helper Functions
+def validate_new_goal_data(request_body):
+    try:
+        title = request_body["title"]
+    except:
+        response = {"details": "Invalid data"}
+        abort(make_response(response, 400))
+    if not isinstance(title, str):
+        response = {"msg": "Invalid book details"}
+        abort(make_response(response, 400))
+
+    return title
