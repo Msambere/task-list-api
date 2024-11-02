9a50260e661eff1be0e256775e3196d4fe077829
Amber <110812972+Msambere@users.noreply.github.com>
2024-11-01 20:30:51 -0500

    Complete wave 5
    Created send_task_completion_slack which connects to Slack API to send
    channel messages when a task is completed.

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,9 +1,10 @@
-
 from flask import Blueprint, abort, make_response, request, Response
 from app.models.task import Task
 from app.db import db
 from sqlalchemy import desc
 import datetime
+import requests
+import os
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
@@ -17,8 +18,9 @@ def create_task():
     db.session.add(new_task)
     db.session.commit()
 
-    response = {"task":new_task.to_dict()}
+    response = {"task": new_task.to_dict()}
     return response, 201
+
 
 @tasks_bp.get("")
 def get_all_tasks():
@@ -31,40 +33,37 @@ def get_all_tasks():
 
     tasks = db.session.scalars(query)
 
-    
     response = [task.to_dict() for task in tasks]
 
     return response, 200
 
+
 @tasks_bp.get("/<task_id>")
 def get_one_task(task_id):
     task = validate_task_id(task_id)
-    return {
-        "task":task.to_dict()
-        }, 200
+    return {"task": task.to_dict()}, 200
+
 
 @tasks_bp.put("/<task_id>")
 def update_task(task_id):
     task = validate_task_id(task_id)
     request_body = request.get_json()
 
-    task.title = request_body['title']
-    task.description = request_body['description']
+    task.title = request_body["title"]
+    task.description = request_body["description"]
     db.session.commit()
 
-    return {
-        "task":task.to_dict()
-        }, 200
-    
+    return {"task": task.to_dict()}, 200
+
+
 @tasks_bp.delete("/<task_id>")
 def delete_task(task_id):
     task = validate_task_id(task_id)
     db.session.delete(task)
     db.session.commit()
-    response = {
-        "details":f"Task {task_id} \"{task.title}\" successfully deleted"
-    }
+    response = {"details": f'Task {task_id} "{task.title}" successfully deleted'}
     return response, 200
+
 
 @tasks_bp.patch("/<task_id>/mark_complete")
 def mark_task_complete(task_id):
@@ -73,9 +72,11 @@ def mark_task_complete(task_id):
     task.completed_at = datetime.datetime.now()
     db.session.commit()
 
-    return {
-        "task":task.to_dict()
-        }, 200
+    notification = send_task_completion_slack(task)
+
+    if notification:
+        return {"task": task.to_dict()}, 200
+
 
 @tasks_bp.patch("/<task_id>/mark_incomplete")
 def mark_task_incomplete(task_id):
@@ -83,35 +84,35 @@ def mark_task_incomplete(task_id):
     task.completed_at = None
     db.session.commit()
 
-    return {
-        "task":task.to_dict()
-        }, 200
+    return {"task": task.to_dict()}, 200
+
 
 # Helper Function
 def validate_task_id(task_id):
     try:
         task_id = int(task_id)
     except:
-        response = {"msg":f"Task id {task_id} is invalid"}
+        response = {"msg": f"Task id {task_id} is invalid"}
         abort(make_response(response, 400))
 
     query = db.select(Task).where(Task.id == task_id)
     found_task = db.session.scalar(query)
 
     if not found_task:
-        response = {"msg":f"Task {task_id} not found."}
+        response = {"msg": f"Task {task_id} not found."}
         abort(make_response(response, 404))
 
     return found_task
-    
+
+
 def validate_new_task_data(request_body):
     try:
-        title = request_body['title']
+        title = request_body["title"]
     except:
         response = {"details": "Invalid data"}
         abort(make_response(response, 400))
     try:
-        description = request_body['description']
+        description = request_body["description"]
     except:
         response = {"details": "Invalid data"}
         abort(make_response(response, 400))
@@ -120,3 +121,15 @@ def validate_new_task_data(request_body):
         abort(make_response(response, 400))
 
     return title, description
+
+def send_task_completion_slack(task):
+    url = "https://slack.com/api/chat.postMessage"
+    api_key = os.environ.get("SLACK_BOT_TOKEN")
+    token = "Bearer "+str(api_key)
+    header = {"Authorization": token}
+    request_body = {
+        "channel": "C07UJK253A7",
+        "text": f"Someone just completed the task {task.title}",
+    }
+
+    return requests.post(url, headers=header, params=request_body)
