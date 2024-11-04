2b84b07c363c416c21ffa222a0a319f729596d7f
Amber <110812972+Msambere@users.noreply.github.com>
2024-11-04 14:28:04 -0600

    Create validate_model() in route_utilitites.py

---
+++ app/routes/route_utilities.py
@@ -0,0 +1,18 @@
+from flask import abort, make_response
+from ..db import db
+
+def validate_model_id(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        response = {"msg": f"{cls.__name__} id {model_id} is invalid"}
+        abort(make_response(response, 400))
+
+    query = db.select(cls).where(cls.id == model_id)
+    found_model = db.session.scalar(query)
+
+    if not found_model:
+        response = {"msg": f"{cls.__name__} {model_id} not found."}
+        abort(make_response(response, 404))
+
+    return found_model
