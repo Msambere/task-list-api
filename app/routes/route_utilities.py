48637c158f1bbe66cb79378686562120c53bf34e
Amber <110812972+Msambere@users.noreply.github.com>
2024-11-05 20:42:22 -0600

    Create get_models_with_filters

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -30,6 +30,20 @@ def create_model(cls, model_data):
     response = {cls.__name__.lower(): new_model.to_dict()}
     return response, 201
 
+def get_models_with_filters(cls, filter_params=None):
+    query = db.select(cls)
+    if filter_params:
+        for param, value in filter_params.items():
+            if param != "order_by" and  hasattr(cls, param):
+                query = query.where(getattr(cls, param).ilike(f"%{value}%"))
+        if "order_by" in filter_params:
+            query = query.order_by(getattr(cls,filter_params["order_by"]))
+
+    models = db.session.scalars(query)
+
+    response = [model.to_dict() for model in models]
+    return response, 200
+    
 
 def delete_record(cls, model_id):
     model = validate_model_id(cls, model_id)
