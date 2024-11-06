985e3d0beda96de535b598f220cf96a963166f22
Amber <110812972+Msambere@users.noreply.github.com>
2024-11-05 18:38:20 -0600

    Update validate_new_model_data error msg

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -1,5 +1,8 @@
 from flask import abort, make_response
+import inspect
 from ..db import db
+from app.models.task import Task
+from app.models.goal import Goal
 
 def validate_model_id(cls, model_id):
     try:
@@ -17,6 +20,17 @@ def validate_model_id(cls, model_id):
 
     return found_model
 
+def create_model(cls, model_data):
+    valid_model_data = validate_new_model_data(cls, model_data)
+
+    new_model = cls.from_dict(valid_model_data)
+    db.session.add(new_model)
+    db.session.commit()
+
+    response = {cls.__name__.lower(): new_model.to_dict()}
+    return response, 201
+
+
 def delete_record(cls, model_id):
     model = validate_model_id(cls, model_id)
     db.session.delete(model)
@@ -26,3 +40,24 @@ def delete_record(cls, model_id):
         "details": f"{cls.__name__} {model.id} \"{model.title}\" successfully deleted"
     }
     return response, 200
+
+#helper function
+def validate_new_model_data(cls, model_data):
+# import inspect
+# class_attributes = inspect.getargspec(Thing).args)
+# class_attributes.pop("id")
+
+    class_attributes = []
+    if cls == Goal or cls == Task:
+        class_attributes.append("title")
+    if cls == Task:
+        class_attributes.append("description")
+
+    for attribute in class_attributes:
+        try:
+            attribute = model_data[attribute]
+        except:
+            response = {"details": "Invalid data"}
+            abort(make_response(response, 400))
+
+    return model_data
